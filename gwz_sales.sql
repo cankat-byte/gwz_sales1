6e191be1cb8392f0ab955e8ac23fe97fd42e2bd8
cankat-byte <cankat1980@gmail.com>
2026-02-14 01:37:05 +0300

    added purchase cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,14 @@
-SELECT
+-- SELECT
      date_date
  , SUM(turnover) AS turnover
  FROM `course14.gwz_sales`
+ GROUP BY date_date
+ ORDER BY date_date
+
+ SELECT
+     date_date
+     , SUM(turnover) AS turnover
+     , SUM(purchase_cost) AS purchase_cost
+ FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
  GROUP BY date_date
  ORDER BY date_date
